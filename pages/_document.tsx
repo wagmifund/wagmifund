339ad5111f9c92dd4bdbfdb70ec7f87fbda7b380
harish-sethuraman <ssharishkumar@gmail.com>
2023-01-02 17:39:35 +0530

    chore: add proper og meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,32 +5,30 @@ import { APP_NAME, DESCRIPTION } from "@utils/constants";
 export default function Document() {
   return (
     <Html>
-      <title>{APP_NAME}</title>
-      <meta name="description" content={DESCRIPTION} />
+      <title>wagmifund</title>
+      <meta name="title" content="wagmifund" />
       <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, maximum-scale=5"
+        name="description"
+        content="Decentralized crowd funding platform, built on Lens Protocol 🌿"
       />
 
-      <link rel="preconnect" href="https://media.lenster.xyz" />
-      <link rel="dns-prefetch" href="https://media.lenster.xyz" />
-
-      <meta property="og:url" content="https://testnet.wagmi.fund" />
-      <meta property="og:site_name" content={APP_NAME} />
-      <meta property="og:title" content={APP_NAME} />
-      <meta property="og:description" content={DESCRIPTION} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://testnet.wagmi.fund/" />
+      <meta property="og:title" content="wagmifund" />
+      <meta
+        property="og:description"
+        content="Decentralized crowd funding platform, built on Lens Protocol 🌿"
+      />
       <meta property="og:image" content="/og.jpeg" />
-      <meta property="og:image:width" content="400" />
-      <meta property="og:image:height" content="400" />
 
-      <meta property="twitter:card" content="summary" />
-      <meta property="twitter:site" content={APP_NAME} />
-      <meta property="twitter:title" content={APP_NAME} />
-      <meta property="twitter:description" content={DESCRIPTION} />
-      <meta property="twitter:image:src" content="/og.jpeg" />
-      <meta property="twitter:image:width" content="400" />
-      <meta property="twitter:image:height" content="400" />
-      <meta property="twitter:creator" content="wagmifund" />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://testnet.wagmi.fund/" />
+      <meta property="twitter:title" content="wagmifund" />
+      <meta
+        property="twitter:description"
+        content="Decentralized crowd funding platform, built on Lens Protocol 🌿"
+      />
+      <meta property="twitter:image" content="/og.jpeg" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
