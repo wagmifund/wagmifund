bc2f8fa3473cc44e8a7f60a779a76cb38db1ee0e
Abhishek Naidu <geekyabhi12@gmail.com>
2023-08-18 02:30:03 +0530

    feat: superfluid integration (#32)
    * chore: monthly subscription powered by superfluid
    
    * chore: fix
    
    * chore: monthly cards for superfluid
    
    * fix eslint
    
    * chore: fix filter length
    
    * chore: about section fix

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,12 +1,19 @@
 import { useAppStore } from "@store/app";
 import { useRouter } from "next/router";
 import Editor from "./Editor";
+import { useProfileUIStore } from "@store/profile";
 const AboutSection = () => {
   const currentProfile = useAppStore((state) => state.currentProfile);
+  const profileUIData = useProfileUIStore((state) => state.profileUIData);
+
   const {
     query: { username },
   } = useRouter();
-  return <Editor isEditable={username === currentProfile?.handle} />;
+  return (
+    profileUIData?.about && (
+      <Editor isEditable={username === currentProfile?.handle} />
+    )
+  );
 };
 
 export default AboutSection;
